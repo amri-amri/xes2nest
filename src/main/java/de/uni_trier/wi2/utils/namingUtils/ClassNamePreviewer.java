393958d738bde9bdb2dbaff8c6f2e252ccac5610
s6kaamri <s6kaamri@uni-trier.de>
2024-04-13 11:19:23 +0200

    Deleted unused OpenXES code

--- src/main/java/de/uni_trier/wi2/utils/namingUtils/ClassNamePreviewer.java
+++ src/main/java/de/uni_trier/wi2/utils/namingUtils/ClassNamePreviewer.java
@@ -1,11 +1,7 @@
 package de.uni_trier.wi2.utils.namingUtils;
 
 import de.uni_trier.wi2.utils.classFactories.*;
-import org.deckfour.xes.factory.XFactoryNaiveImpl;
-import org.deckfour.xes.in.XesXmlParser;
-import org.deckfour.xes.model.*;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -15,60 +11,8 @@ import java.util.List;
  */
 public class ClassNamePreviewer {
 
-    final private XLog log;
     private List<String> createdClasses = new ArrayList<>();
 
-    /**
-     * Creates a new Class name previewer.
-     *
-     * @param filepath path to the xes file, from which the XES-files shall be abstracted
-     * @throws Exception
-     */
-    public ClassNamePreviewer(String filepath) throws Exception {
-        XFactoryNaiveImpl xFactory = new XFactoryNaiveImpl();
-        XesXmlParser xmlParser = new XesXmlParser(xFactory);
-        log = xmlParser.parse(new File(filepath)).get(0);
-    }
-
-    /**
-     * Prints the class names of the classes that will be created when converting the XES file a NEST Graph.
-     */
-    public void printClassNames() {
-        for (XAttribute attribute : log.getGlobalEventAttributes()) {
-            printAttribute(attribute);
-        }
-        for (XAttribute attribute : log.getGlobalTraceAttributes()) {
-            printAttribute(attribute);
-        }
-        for (int i = 0; i < log.size(); i++) {
-            XTrace xTrace = log.get(i);
-            printAttributeMap(xTrace.getAttributes());
-            for (int j = 0; j < xTrace.size(); j++) {
-                XEvent xEvent = xTrace.get(j);
-                printAttributeMap(xEvent.getAttributes());
-            }
-        }
-    }
-
-    private void printAttributeMap(XAttributeMap attributes) {
-        for (XAttribute current : attributes.values()) {
-            printAttribute(current);
-        }
-    }
-
-    private void printAttribute(XAttribute attribute) {
-        String className = createClassName(attribute);
-        if (!createdClasses.contains(className)) {
-            createdClasses.add(className);
-            System.out.println(attribute.getKey() + " -> " + className);
-        }
-        printAttributeMap(attribute.getAttributes());
-
-    }
-
-    private String createClassName(XAttribute attr) {
-        return KeyNameConverter.getValidName(attr.getKey()) + getClassOfType(attr.getClass().getSimpleName());
-    }
 
     private String getClassOfType(String attrClassName) {
         String className;
@@ -109,12 +53,6 @@ public class ClassNamePreviewer {
         return className;
     }
 
-    /**
-     * Prints the names of the classes that were predicted by preview and which do not exist in "names" and vice versa.
-     * {@link ClassNamePreviewer#printClassNames()} must be run before using the method.
-     *
-     * @param names Class names to which the preview should be compared.
-     */
     public void getDifferingClasses(Collection<String> names) {
         System.out.println("Differing Classes");
         System.out.println("Actually created but missing in Preview:");
